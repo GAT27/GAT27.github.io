2222619092449f8cf13ae319b0f468be1ca4b76d
Guillermo Alfredo Torijano <shstudent1121@yahoo.com>
2015-05-05 10:21:10 -0400

    Site Update
    Reworked the design of some things, also long time commit.

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ var main = function()
 		var fl = front.last().parent().children().index(front.last());
 		$(".pane>li").eq(ff)
 		.replaceWith(front.last().parent().siblings().children()
-					 .eq(fl).clone().removeClass("splash"));
+					 .eq(fl).clone()/*.removeClass("splash")*/);
 	}
 	
 	//Prepare page layout
@@ -23,22 +23,26 @@ var main = function()
 	{	$(this).children().first().siblings().hide();
 	});
 	$(".pane>li>div").each(function()
-	{	$(this).prepend($(this).children("div").children().first().clone()
-						.css({"width":"55%","height":"85%","float":"left"}));
+	{	$(this).prepend($(this).children(".minipic").children().first().clone()
+						.css({"width":"65%","height":"50%"}));
 	});
+	$("#navbar>li:eq(0)").css({"color":"#10001f","background-color":"#099696"});
+	$("#navbar>li:eq(1)").css({"color":"#802e19","background-color":"#b39d5d"});
+	$("#navbar>li:eq(2)").css({"color":"#59ab4b","background-color":"#660a00"});
 	var marker_pillar_color = reskin(location.hash);
 	var marker = marker_pillar_color[0];
 	var pillar = marker_pillar_color[1];
 	var color1 = marker_pillar_color[2];
 	var color2 = marker_pillar_color[3];
 	pillar.toggleClass("pillar");
-	var minipic = $(".pane>li>div>div>*");
+	var minipic = $(".minipic>*");
 	var reference = $("#footer>a");
 	reference.each(function()
 	{	$(this).children().first().siblings().hide();
 	});
-	$(".flip>img").first().css({"float":"left"});
-	$(".flip>img").last().css({"float":"right"});
+	$(".flip>img").eq(1).css({"float":"right"});
+	//$(".flip>img").last().css({"float":"right"});
+	//$(".flip>img").first().css({"display":"block"});
 	$("#load").remove();
 	
 	//Reset page on navigation change
@@ -59,31 +63,32 @@ var main = function()
 	{	if ($(this).parent().hasClass("flip"))
 		{	var col = $(".pane>li").first().children().finish();
 			var current = col.first();
-			if ($(this).is($(".flip>img").eq(0)))
+			if ($(this).is($(".flip>img").eq(2)))
 			{	var upnext = current;
 				if (current.children().attr("src").length)
 					window.open(current.children().attr("src"),"_blank");
 			}
-			else if ($(this).is($(".flip>img").eq(1)))
+			else if ($(this).is($(".flip>img").eq(0)) && col.length!=1)
 			{	var upnext = col.last();
 				current.before(upnext);
 			}
-			else if ($(this).is($(".flip>img").eq(2)))
+			else if ($(this).is($(".flip>img").eq(1)) && col.length!=1)
 			{	var upnext = current.next();
 				col.last().after(current);
 			}
 			else
-			{	var escape = 0;
+			{	/*var escape = 0;
 				while (marker.index(current) != (-1+col.length))
 				{	col.last().after(current.hide());
 					col = $(".pane>li").first().children();
 					current = col.first().show();
 					escape++; if (escape>20) {alert("fail");break;}
-				}
+				}*/
 				var upnext = current;
 			}
 			current.fadeOut(0,function(){upnext.fadeIn(0);});
-			$(".flip>b").text(1+marker.index(upnext) + " / " + col.length);
+			$(".pane").find("div").first().children().first().after($(".flip"));
+			$(".flip b").text(1+marker.index(upnext) + " / " + col.length);
 		}
 		//else
 			//marker = colscroll(!$(this).is($(".scroll>img").last()),pillar);
@@ -93,19 +98,26 @@ var main = function()
 	minipic.mouseenter(function()
 	{	$(this).closest("li>div").children().first().remove();
 		$(this).closest("li>div").prepend($(this).clone()
-										  .css({"width":"55%","height":"85%","float":"left"}));
+										  .css({"width":"65%","height":"50%"}));
 	});
 	minipic.click(function()
 	{	window.open($(this).attr("src"),"_blank");
 	});
 	
 	//Pillar navigation by up or down
-	$("body").on("click",".pillar",function()
+	$("body").on({click:function()
 	{	var col = $("div.column").first().children();
 		if ($(this).is(col.first()) || $(this).is($(".scroll").next()))
 			marker = colscroll(!$(this).is(col.first()),pillar,color1,color2);
-		return false;
-	});
+		$(this).css("text-shadow","none");
+	},
+	mouseenter:function()
+	{	$(this).not(".inpillar").css("text-shadow","2px 2px 2px white");
+	},
+	mouseleave:function()
+	{	$(this).css("text-shadow","none");
+	}
+	},".pillar",false);
 	
 	//Pillar navigation by tabs
 	$("body").on({click:function()
@@ -125,7 +137,7 @@ var main = function()
 	mouseenter:function()
 	{	$(this).animate({right:"400%",width:"500%"},"fast")
 		.append(pillar.eq($(".scroll>div").index($(this))).children("h1").clone()
-				.css("font-size","2vw"));
+				.css({"font-size":"2vw","margin":"0"}));
 	},
 	mouseleave:function()
 	{	$(this).animate({right:"0%",width:"100%"},"fast")
@@ -135,6 +147,7 @@ var main = function()
 	
 	//Pillar navigation by mouse wheel
 	//http://www.sitepoint.com/html5-javascript-mouse-wheel/
+	var scroll_lk = 1;
 	if (window.addEventListener) 
 	{	window.addEventListener("mousewheel",wheelscroll,false);
 		window.addEventListener("DOMMouseScroll",wheelscroll,false);
@@ -142,12 +155,21 @@ var main = function()
 	else
 		window.attachEvent("onmousewheel",wheelscroll);
 	function wheelscroll(e)
-	{	var e = window.event || e;
-		var delta = Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail)));
-		marker = colscroll(delta-1,pillar,color1,color2);
+	{	if (!scroll_lk)
+		{	var e = window.event || e;
+			var delta = Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail)));
+			marker = colscroll(delta-1,pillar,color1,color2);
+			scroll_lk++;
+		}
+		else
+		{	scroll_lk++;
+			if (scroll_lk == 15)
+				scroll_lk = 0;
+		}
 		return false;
 	};
 	
+	//Page navigation
 	$("#navbar>li").click(function()
 	{	if ($(this).hasClass("home"))
 			location.href = "#home";
@@ -155,16 +177,20 @@ var main = function()
 			location.href = "#projects";
 		else if ($(this).hasClass("social"))
 			location.href = "#social";
-		else if ($(this).is($("#navbar>li:nth-child(4)")))
-			$("#footer").show(0,function(){$(this).animate({top:"0"},"fast");});
 	});
-	
-	$("#footer>h2").click(function()
-	{	$("#footer").animate({top:"101%"},"fast",function(){$(this).hide();});
+	$("#help").click(function()
+	{	$("#footer").show(0,function()
+		{	$(this).animate({right:"25%"},"fast",function()
+			{	$(this).mouseleave(function()
+				{	$(this).animate({right:"101%"},"fast",function(){$(this).hide();});
+				});
+			});
+		});
 	});
 	
 	reference.hover(function()
-	{	$(this).children().first().fadeOut(0);//.css({"z-index":"0","position":"relative"});
+	{	$(this).children().first().fadeOut(0);
+		//.css({"z-index":"0","position":"relative"});
 		$(this).children().first().siblings().fadeIn(0);
 	},function()
 	{	$(this).children().first().fadeIn(0);
@@ -182,12 +208,13 @@ var reskin = function(upfront)
 	
 	//Reorder navbar and safetly move pane controls
 	var nav = $("#navbar>li");
+	//?//$().toggleClass();
 	if (upfront == "#"+nav.first().attr("class"))
 		nav.first().toggleClass()
-		.before(nav.last().after(nav.eq(2).toggleClass())).insertAfter(nav.eq(1));
+		.before(nav.last().after(nav.eq(1).toggleClass()));//.insertAfter(nav.eq(1));
 	else if (upfront == "#"+nav.last().attr("class"))
 		nav.last().toggleClass()
-		.after(nav.first().before(nav.eq(2).toggleClass())).insertBefore(nav.eq(3));
+		.after(nav.first().before(nav.eq(1).toggleClass()));//.insertBefore(nav.eq(3));
 	$("#footer").before($(".flip"));
 	
 	//Select new page theme
@@ -200,7 +227,7 @@ var reskin = function(upfront)
 	else if (upfront == "#social")
 	{	upfront = $(upfront);
 		var color1 = "#660a00";
-		var color2 = "#7cc96f";
+		var color2 = "#59ab4b";
 		$("head>title").text("GATq SOCIAL");
 	}
 	else
@@ -211,6 +238,7 @@ var reskin = function(upfront)
 	}
 	$("body").css({"color":color2,"background-color":color1});
 	$(".column").css({"color":color1,"background-color":color2});
+	$(".ref>a").css({"color":color2,"background-color":color1});
 	
 	//Return previous column to its original state
 	//$(".pane").first().children("li").first().fadeOut(600);
@@ -233,7 +261,7 @@ var reskin = function(upfront)
 		.before($("ul.pane").first().children("li").last());
 	}
 	else
-		nav.eq(2).toggleClass();
+		nav.eq(1).toggleClass();
 	
 	//Show new page theme and get new counters and title
 	$("#header").after(upfront.show());
@@ -244,17 +272,15 @@ var reskin = function(upfront)
 	marker.first().show().parent().show();
 	$("#header>*").replaceWith(pillar.first().children("h1"));
 	
-	//Set up column view
+	//Set up pillar view
 	pillar.first().show().addClass("inpillar").next().show().prev()
 	.after($(".scroll").css({"width":"20%","height":"70%",
 							 "display":"inline-block"}))
 	.before(pillar.last().show());
 	//.end().first().children().show();
 	pillar.first().children().show();
-	//alert(pillar.eq(8).width());//.offsetWidth);
-	//alert(pillar.eq(8).children("h1").textWidth());
 	
-	//Set up side tabs
+	//Set up column tabs
 	var h = (100/pillar.length) + "%";
 	$(".scroll").children().remove().end().append($("<div>").text(1)
 	.css({"width":"100%","height":h,"position":"relative"}));
@@ -269,10 +295,11 @@ var reskin = function(upfront)
 	{	for (upsplit--;upsplit>0;upsplit--)
 			marker = colscroll(true,pillar,color1,color2);
 	}
+	//$(".flip").css("background-color",color1);
 	//$(".pane").first().append($(".flip").show());
-	//alert($(".pane").first().text());
-	$(".pane").first().after($(".flip").show());
-	$(".flip>b").text("1 / " + marker.length);
+	$(".pane").find("div").first().children().first().after($(".flip").show());
+	//$(".pane").first().after($(".flip").show());
+	$(".flip b").text("1 / " + marker.length);
 	//$(".scroll>b").text("1 / " + pillar.length);
 	
 	return [marker,pillar,color1,color2];
@@ -325,7 +352,8 @@ var colscroll = function(action,pillar,color1,color2)
 	c.first().show().siblings().hide();
 	var marker = $(".pane>li").first().children();
 	current.fadeOut(0,function(){upnext.fadeIn(0);});
-	$(".flip>b").text("1 / " + marker.length);
+	$(".pane").find("div").first().children().first().after($(".flip"));
+	$(".flip b").text("1 / " + marker.length);
 	
 	return marker;
 };
